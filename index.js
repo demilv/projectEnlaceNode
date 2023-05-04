fc63216c74f9e500b551701d8703925bff9b40f8
demilv <gonzalo.cano@bootcamp-upgrade.com>
2023-05-04 20:04:19 +0200

    el coso

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config();
+const {connect} = require("./src/utils/database")
+
+//Importe de rutas de las colecciones
+const routerMovie = require("./src/api/routes/movie.routes"); 
+const routerCine = require("./src/api/routes/cine.routes");
+
+
+const app = express();
+connect(); 
+app.use(cors());
+app.use(express.json())
+const PORT = process.env.PORT || 5000
+
+app.use("/movie", routerMovie)
+app.use("/cine", routerCine)
+
+app.listen(PORT, () =>{
+    console.log(`URL del servidor : http://localhost:${PORT}`)
+})
+
